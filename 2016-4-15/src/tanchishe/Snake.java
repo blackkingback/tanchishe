eb09a155ece15d9441a3b9f39ff5690f33085fa1
blackkingback <33114630+blackkingback@users.noreply.github.com>
2017-11-03 19:21:02 -0700

    Update Snake.java

--- 2016-4-15/src/tanchishe/Snake.java
+++ 2016-4-15/src/tanchishe/Snake.java
@@ -84,7 +84,7 @@ public class Snake extends JFrame implements ActionListener {
 			neweGame.setEnabled(false);
 			break;
 		case "high":
-			she.speed = She.HIGH;
+			she.speed = She.MIDDLE;
 			break;
 		case "medium":
 			she.speed = She.MIDDLE;
